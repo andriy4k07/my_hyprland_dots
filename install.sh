241e5274c1c3e1012f2673186fd25de8cf715360
Andriy4k <for.it.ak07@gmail.com>
2025-08-08 21:11:11 +0300

    update script

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ network_bt=(
 )
 
 utilities=(
-  brightnessctl btop htop cliphist evtest micro nano vim uwufetch unrar unzip rofi nemo kitty telegram-desktop mako ttf-jetbrains-mono-nerd swww  noto-fonts-emoji less
+  brightnessctl btop htop cliphist evtest micro nano vim uwufetch unrar unzip rofi nemo kitty telegram-desktop mako ttf-jetbrains-mono-nerd swww  noto-fonts-emoji less signal-desktop
 )
 
 media_graphics=(
